ada2dd454e66178f2128ef7ecab82ea819a2d28d
iamshubham16 <guglanishubham16@gmail.com>
2020-03-30 01:03:18 +0530

    Adding Ajax requests using Axios

--- src/containers/BurgerBuider/BurgerBuilder.js
+++ src/containers/BurgerBuider/BurgerBuilder.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import Aux from '../../hoc/ReactAux/ReactAux'
-import Burger from './../../components/Burger/Burger'
-import BuildControls from '../../components/Burger/BuildControls/BuildControls'
-import Modal from '../../components/UI/Modal/Modal'
-import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
-
+import Aux from '../../hoc/ReactAux/ReactAux';
+import Burger from './../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 const INGREDIENT_PRICES = {
     salad: 15,
@@ -23,7 +24,8 @@ class BurgerBuilder extends Component {
         }, 
         totalPrice: 20,
         purchaseable: false,
-        purchasing: false
+        purchasing: false,
+        loading: false
     }
 
     updatePurchaseState (ingredients) {
@@ -88,7 +90,38 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continued');
+        this.setState({
+            loading: true
+        });
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Shubham Guglani',
+                address: {
+                    apartment: 'C-86, Millennium Apartments',
+                    zipCode: '110089',
+                    city: 'Delhi',
+                    country: 'India'
+                },
+                email: 'guglanishubham16@gmail.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order)
+            .then(response => {
+                this.setState({
+                    loading: false,
+                    purchasing: false
+                });
+            })
+            .catch(error => {
+                this.setState({
+                    loading: false,
+                    purchasing: false
+                });
+
+            });
     }
 
     render() {
@@ -98,13 +131,17 @@ class BurgerBuilder extends Component {
         for(let key in disabledInfo) {
             disabledInfo[key] = disabledInfo[key] <= 0
         }
+        let orderSummary = <OrderSummary ingredients = {this.state.ingredients}
+                price = {this.state.totalPrice}  
+                purchaseCancelled = {this.purchaseCancelHandler}
+                purchaseContinued = {this.purchaseContinueHandler} />; 
+        if(this.state.loading) {
+            orderSummary = <Spinner />;
+        }        
         return (
             <Aux>
                 <Modal show = {this.state.purchasing} modalClosed = {this.purchaseCancelHandler}>
-                    <OrderSummary ingredients = {this.state.ingredients}
-                                  price = {this.state.totalPrice}  
-                                  purchaseCancelled = {this.purchaseCancelHandler}
-                                  purchaseContinued = {this.purchaseContinueHandler} />
+                    {orderSummary}
                 </Modal>
                 <Burger ingredients = {this.state.ingredients}/>
                 <BuildControls
