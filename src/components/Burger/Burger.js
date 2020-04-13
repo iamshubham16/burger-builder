d670d4caf9e57a292f9213077fc47767412593c5
iamshubham16 <guglanishubham16@gmail.com>
2020-04-14 04:23:54 +0530

    Outputing the orders

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,7 +6,7 @@ import BurgerIngredient from './BurgerIngredients/BurgerIngredients'
 
 
 const burger = (props) => {
-    console.log(props);
+
     let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
