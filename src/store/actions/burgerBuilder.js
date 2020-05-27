1cd615aea779fdbd3d951985aa02c28d9e839d42
iamshubham16 <guglanishubham16@gmail.com>
2020-05-28 02:31:21 +0530

    Changing the order of our ingredients manually

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -30,7 +30,7 @@ export const fetchIngredientsFailed = () => {
 
 export const initIngredients = () => {
     return dispatch => {
-        axios.get('https://react-brawsis-burger.firebaseio.com/ingredie')
+        axios.get('https://react-brawsis-burger.firebaseio.com/ingredients.json')
             .then(response => {
                 dispatch(setIngredients(response.data));
             })
