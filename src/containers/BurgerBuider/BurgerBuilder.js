49955f61183dbc20545a8d4cf21282507e8d9304
iamshubham16 <guglanishubham16@gmail.com>
2020-05-28 02:01:27 +0530

    Executing Async code

--- src/containers/BurgerBuider/BurgerBuilder.js
+++ src/containers/BurgerBuider/BurgerBuilder.js
@@ -21,17 +21,18 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount () {
-        // axios.get('https://react-brawsis-burger.firebaseio.com/ingredients.json')
-        //     .then(response => {
-        //         this.setState({
-        //             ingredients: response.data
-        //         })
-        //     })
-        //     .catch(error => {
-        //         this.setState({
-        //             error: true
-        //         })
-        //     });
+        console.log(this.props);
+        axios.get('https://react-brawsis-burger.firebaseio.com/ingredients.json')
+            .then(response => {
+                this.setState({
+                    ingredients: response.data
+                })
+            })
+            .catch(error => {
+                this.setState({
+                    error: true
+                })
+            });
     }
 
     updatePurchaseState (ingredients) {
