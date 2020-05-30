f206b72dd5a5002d603fdefde283a4ea3ce18039
iamshubham16 <guglanishubham16@gmail.com>
2020-05-30 21:23:11 +0530

    Combining reducers

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -106,7 +106,7 @@ class ContactData extends Component {
                             {value:'ordinary', displayValue: 'Ordinary'}
                         ]
                     },
-                    value: '',
+                    value: 'fastest',
                     validation:{},
                     valid: true
                 }
@@ -207,9 +207,9 @@ class ContactData extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.ingredients,
-        price: state.totalPrice,
-        loading: state.loading
+        ings: state.burgerBuilder.ingredients,
+        price: state.burgerBuilder.totalPrice,
+        loading: state.order.loading
     }
 };
 
