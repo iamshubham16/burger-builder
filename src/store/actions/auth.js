3d0cb071dadeaebfba718d2a439ad06496c38102
iamshubham16 <guglanishubham16@gmail.com>
2020-06-01 02:01:37 +0530

    Adding Sign in

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,7 +22,7 @@ export const authFail = (error) => {
     }
 }
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
         const authData = {
@@ -30,7 +30,11 @@ export const auth = (email, password) => {
             password: password,
             returnSecureToken: true
         }
-        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCPLP0GMCuDVPlTHpJmYIe1fc5uEuBJWLM', authData)
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCPLP0GMCuDVPlTHpJmYIe1fc5uEuBJWLM';
+        if(!isSignup) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCPLP0GMCuDVPlTHpJmYIe1fc5uEuBJWLM';
+        }
+        axios.post(url, authData)
             .then( response => {
                 console.log(response);
                 dispatch(authSuccess(response.data))
