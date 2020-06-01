6c2f4e5f640883d7dd685a10d2ea9d796bcae9d6
iamshubham16 <guglanishubham16@gmail.com>
2020-06-01 21:48:23 +0530

    Guarding routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
@@ -19,21 +19,41 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <div>
-        <Layout>
-          <Switch>
+    let routes = (
+      <Switch>
+        <Route path = "/auth" component = {Auth} />
+        <Route path = "/" exact component = {BurgerBuilder}/>
+        <Redirect to ="/" />
+      </Switch>
+    );
+
+    if(this.props.isAutheticated) {
+      routes = (
+        <Switch>
             <Route path = "/checkout" component = {Checkout}/>  
             <Route path = "/orders" component = {Orders} />
             <Route path = "/auth" component = {Auth} />
             <Route path = "/logout" component = {Logout} />
             <Route path = "/" exact component = {BurgerBuilder}/>
-          </Switch>
+            <Redirect to ="/" />
+        </Switch>
+      );
+    }
+    return (
+      <div>
+        <Layout>
+          {routes}    
         </Layout>
       </div>
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    isAutheticated: state.auth.token != null
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
@@ -41,4 +61,4 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
